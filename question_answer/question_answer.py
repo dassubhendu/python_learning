9fd7b3cc2478138a53f10b926a409d5f61447309
Subhendu Das <subhendu.das@lexmark.com>
2024-01-16 21:42:23 +0530

    Updated

--- question_answer/question_answer.py
+++ question_answer/question_answer.py
@@ -52,7 +52,7 @@ print(" ***************** Can we modify a tuple while lies inside a list? ******
 list001 = [10, True, "Hello", (1, 2, 3)]
 print("list001 which contains tuple: ", list001)
 print("tuple inside list001: ", list001[3][1])
-#list001[3][1] = 100
+# list001[3][1] = 100
 print("list001 after modifying tuple: ", list001)
 list001[3] = (4, 5, 6)
 print("list001 after modifying tuple: ", list001)
@@ -120,6 +120,7 @@ print("\n")
 """
 print(" ***************** Randomize the items of a list in place in Python *****************")
 from random import shuffle
+
 list002 = [10, 20, 30, 40, 50]
 print("list002 before randomizing: ", list002)
 shuffle(list002)
@@ -171,7 +172,7 @@ print("\n")
 --> Dictionary comprehension consists of an expression pair (key: value) followed by for statement inside curly braces {}.
 """
 print(" ***************** Dictionary comprehension *****************")
-myDict = {x: x**2 for x in [1, 2, 3, 4, 5]}
+myDict = {x: x ** 2 for x in [1, 2, 3, 4, 5]}
 print("myDict: ", myDict)
 
 print("\n")
@@ -297,7 +298,165 @@ print("list006: ", list006)
 numbers = ','.join(str(i) for i in list006)
 print("numbers: ", numbers)
 
+print("\n")
+
+"""
+23. Take the below two lists as input and print expected output.
+    list1 = ['My', 'name']
+    list2 = ['is', 'John']
+    Expected output: My name is John
+"""
+list1 = ['My', 'name']
+list2 = ['is', 'John']
+print(" ***************** Take the below two lists as input and print expected output. *****************")
+list1.extend(list2)
+print(' '.join(list1))
+
+print("\n")
+
+"""
+24. Covert two lists into a dictionary.
+"""
+list3 = ['a', 'b', 'c']
+list4 = [1, 2, 3]
+list5 = dict(zip(list3, list4))
+print("list5: ", list5)
+
+# Using dictionary comprehension
+list6 = {list3[i]: list4[i] for i in range(len(list3))}
+print("list6: ", list6)
+
+# Using for loop
+list7 = {}
+for key in list3:
+    for value in list4:
+        list7[key] = value
+        list4.remove(value)
+        break
+print("list7: ", list7)
+
+"""
+25. Write a program to count the number of times a class is called
+"""
+print(" ***************** Write a program to count the number of times a class is called *****************")
 
 
+class A:
+    count = 0
+
+    def __init__(self):
+        A.count += 1
+        print("Count: ", A.count)
 
 
+a1 = A()
+a2 = A()
+print(A.count)
+
+"""
+26. Write a program to find duplicate characters in a list
+"""
+list8 = ['India', 'is', 'my', 'country']
+str1 = ''.join(list8)
+print("str1: ", str1)
+duplicates = []
+for char in str1:
+    if str1.count(char) > 1:
+        if char not in duplicates:
+            duplicates.append(char)
+print("duplicates: ", duplicates)
+print(*duplicates)  # * is used to print the list without brackets and commas
+
+print("\n")
+
+"""
+27. Write a program to find the sum of all the numbers in a list
+"""
+list9 = [10, 20, 30, 40, 50]
+sum1 = 0
+for i in list9:
+    sum1 += i
+print("sum1: ", sum1)
+
+print("\n")
+
+"""
+28. Write a program to find the sum of all the even numbers in a list
+"""
+list10 = [10, 21, 30, 40, 50]
+sum2 = 0
+for i in list10:
+    if i % 2 == 0:
+        sum2 += i
+print("sum2: ", sum2)
+
+"""
+29. Write a program to find the sum of all the odd numbers in a list
+"""
+list11 = [10, 21, 30, 40, 50]
+sum3 = 0
+for i in list11:
+    if i % 2 != 0:
+        sum3 += i
+print("sum3: ", sum3)
+
+print("\n")
+
+"""
+30. Write a program to find unique characters in a string
+"""
+str2 = "Hello World"
+unique = []
+for char in str2:
+    if str2.count(char) == 1 and char not in unique:
+        unique.append(char)
+print("unique: ", unique)
+print(*unique)
+
+print("\n")
+
+"""
+31. What would be the output of the following code?
+--> MRO (Method Resolution Order) is the order in which base classes are searched when executing a method.
+"""
+print(" ***************** MRO (Method Resolution Order) *****************")
+
+
+class A1:
+    def demo(self):
+        print("In class A1")
+
+
+class B1(A1):
+    def demo(self):
+        print("In class B1")
+
+
+class C1(A1):
+    def demo(self):
+        print("In class C1")
+
+
+class D1(B1, C1):
+    pass
+
+
+test = D1()
+test.demo()
+
+print("\n")
+
+"""
+32. Write a one line for loop to print all the words in a list which starts with 'i'
+"""
+print(" ***************** Write a one line for loop to print all the words in a list which starts with 'i' *****************")
+list12 = ['india', 'is', 'my', 'country']
+print([word for word in list12 if word.startswith('i')])
+
+print("\n")
+
+"""
+33. What is the difference between is and ==
+--> is ==> compares the identity of two objects. Reference / address comparison and returns True or False.
+--> == ==> compares the values of two objects. Content comparison and returns True or False.
+"""
